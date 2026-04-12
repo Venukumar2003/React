998c971e2242793be31b493e2f1c7aa0e5762da9
Venu <muddangulavenukumar@gmail.com>
2026-04-12 16:03:23 +0530

    practice fetching and filterig and sorting data in table format using react

--- CreateTable/table/src/Table.jsx
+++ CreateTable/table/src/Table.jsx
@@ -76,21 +76,132 @@
 
 
 
+// import React, { useState, useEffect } from "react";
+
+
+
+// const Table = () => {
+
+//     const [data, setData] = useState([]);
+//     const [search, setSearch] = useState("");
+//     const [sortOrder, setSortOrder] = useState("")
+
+//     useEffect(() => {
+//         const fetchData = async () => {
+//             try {
+//                 const userData = await fetch('https://jsonplaceholder.typicode.com/users')
+//                 const result = await userData.json();
+//                 console.log(result);
+
+//                 setData(result);
+
+//             } catch (error) {
+//                 console.log(error)
+//             }
+
+//         }
+//         fetchData();
+//     }, [])
+
+//     const filterUsers = data.filter((data) => (
+//         data.name.toLowerCase().includes(search.toLowerCase())
+
+//     ))
+//     console.log("filterUsers-------", filterUsers)
+
+
+//     const sortedUsers = [...filterUsers].sort((a, b) => {
+//         if (sortOrder === "asc") {
+//             return a.name.localeCompare(b.name)
+//         } else {
+//             return b.name.localeCompare(a.name)
+//         }
+//     })
+
+//     console.log("sorted Users-------", sortedUsers)
+
+//     return (
+//         <>
+//             <h2> User Table </h2>
+
+//             {data ? <p> Data Loaded</p> : <p>Loading....</p>}
+
+//             <label htmlFor="search"> Filter :</label>
+//             <input type="text"
+//                 value={search}
+//                 placeholder="Search by Name"
+//                 name="search"
+//                 onChange={(e) => setSearch(e.target.value)} />
+
+
+//             <button onClick={() => setSortOrder("asc")}> sort A-Z</button>
+//             <button onClick={() => setSortOrder("desc")}> sort Z-A</button>
+
+
+
+//             <table border="2" cellPadding="5">
+//                 <thead>
+//                     <tr>
+//                         <th>ID</th>
+//                         <th>Name</th>
+//                         <th>Email</th>
+//                         <th>City</th>
+//                     </tr>
+
+//                 </thead>
+
+
+
+//                 <tbody>
+//                     {sortedUsers.map((user) => (
+//                         <tr key={user.id}>
+//                             <td>{user.id}</td>
+//                             <td>{user.name}</td>
+//                             <td>{user.email}</td>
+//                             <td>{user.address.city}</td>
+//                         </tr>
+//                     ))}
+
+//                 </tbody>
+
+//             </table>
+
+
+
+
+
+
+//         </>
+//     )
+
+// }
+
+// export default Table
+
+
+
 import React, { useState, useEffect } from "react";
 
 
 
 const Table = () => {
 
+
     const [data, setData] = useState([]);
-    const [search, setSearch] = useState("");
-    const [sortOrder, setSortOrder] = useState("")
+    const [search, setSearch] = useState("")
+    const [sortData, setSortData] = useState("")
+
+
+
 
     useEffect(() => {
+
         const fetchData = async () => {
             try {
-                const userData = await fetch('https://jsonplaceholder.typicode.com/users')
-                const result = await userData.json();
+                const response = await fetch("https://jsonplaceholder.typicode.com/users")
+
+
+                const result = await response.json();
                 console.log(result);
 
                 setData(result);
@@ -100,80 +211,82 @@ const Table = () => {
             }
 
         }
+
         fetchData();
+
     }, [])
 
-    const filterUsers = data.filter((data) => (
-        data.name.toLowerCase().includes(search.toLowerCase())
 
-    ))
-    console.log("filterUsers-------", filterUsers)
+    const filterUsers = data.filter((data) => (
+        data.name.toLocaleLowerCase().includes(search.toLowerCase())
+    ));
+
+
+    console.log("filterd users", filterUsers)
 
 
     const sortedUsers = [...filterUsers].sort((a, b) => {
-        if (sortOrder === "asc") {
+
+        if (sortData === "asc") {
             return a.name.localeCompare(b.name)
         } else {
             return b.name.localeCompare(a.name)
         }
-    })
+    });
 
-    console.log("sorted Users-------", sortedUsers)
+    console.log("sorted users", sortedUsers);
+
 
     return (
         <>
-            <h2> User Table </h2>
+            <h2> Users Table </h2>
 
-            {data ? <p> Data Loaded</p> : <p>Loading....</p>}
+            {data ? <div> Data Loaded</div> : <div> Loading....</div>}
 
-            <label htmlFor="search"> Filter :</label>
+            <label htmlFor="search">Filter :</label>
             <input type="text"
-                value={search}
-                placeholder="Search by Name"
+                placeholder="Filter by Name"
                 name="search"
+                value={search}
                 onChange={(e) => setSearch(e.target.value)} />
 
 
-            <button onClick={() => setSortOrder("asc")}> sort A-Z</button>
-            <button onClick={() => setSortOrder("desc")}> sort Z-A</button>
 
+            <button onClick={() => setSortData("asc")}> sort A-Z </button>
+            <button onClick={() => setSortData("desc")}> sort Z-A</button>
 
-
-            <table border="2" cellPadding="5">
+            <table border="2" cellpadding="5">
                 <thead>
                     <tr>
-                        <th>ID</th>
                         <th>Name</th>
-                        <th>Email</th>
+                        <th>Email </th>
+                        <th>Phone No </th>
                         <th>City</th>
+
                     </tr>
 
                 </thead>
 
-
-
                 <tbody>
+
                     {sortedUsers.map((user) => (
                         <tr key={user.id}>
-                            <td>{user.id}</td>
-                            <td>{user.name}</td>
+
+                            <td>{user.name} </td>
                             <td>{user.email}</td>
+                            <td>{user.phone}</td>
                             <td>{user.address.city}</td>
                         </tr>
+
                     ))}
 
                 </tbody>
 
+
             </table>
-
-
-
-
-
 
         </>
     )
-
 }
 
-export default Table
+export default Table;
